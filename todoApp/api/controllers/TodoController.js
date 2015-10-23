907e7893ca6cc6ae4dc53ddba1f115abec651058
nathanielgreen <ngreen2303@gmail.com>
2015-10-23 15:36:51 +0100

    initialised rails server and added code. base todo list working

---
+++ todoApp/api/controllers/TodoController.js
@@ -0,0 +1,19 @@
+module.exports = {
+    getTodos: function(req, res) {
+        TodoService.getTodos(function(todos) {
+            res.json(todos);
+        });
+    },
+    addTodo: function(req, res) {
+        var todoVal = (req.body.value) ? req.body.value : undefined
+        TodoService.addTodo(todoVal, function(success) {
+            res.json(success);
+        });
+    },
+    removeTodo: function(req, res) {
+       var todoVal = (req.body.value) ? req.body.value : undefined
+        TodoService.removeTodo(todoVal, function(success) {
+            res.json(success);
+        });
+    }
+};
